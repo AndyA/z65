20c5036b5118f3d2e8d306699352db7c7dae9b91
Andy Armstrong <andy@hexten.net>
2025-07-17 19:56:56 +0100

    Doesn't need to be a cpu.

--- src/cpu_tools.zig
+++ src/cpu_tools.zig
@@ -9,34 +9,34 @@ pub fn getXY(cpu: anytype) u16 {
     return @as(u16, cpu.Y) << 8 | @as(u16, cpu.X);
 }
 
-pub fn pokeBytes(cpu: anytype, addr: u16, bytes: []const u8) void {
+pub fn pokeBytes(mem: anytype, addr: u16, bytes: []const u8) void {
     for (bytes, 0..) |byte, i| {
         const offset: u16 = @intCast(i);
-        cpu.poke8(addr + offset, byte);
+        mem.poke8(addr + offset, byte);
     }
 }
 
-pub fn peekBytes(cpu: anytype, addr: u16, bytes: []u8) void {
+pub fn peekBytes(mem: anytype, addr: u16, bytes: []u8) void {
     for (0..bytes.len) |i| {
         const offset: u16 = @intCast(i);
-        bytes[i] = cpu.peek8(addr + offset);
+        bytes[i] = mem.peek8(addr + offset);
     }
 }
 
-pub fn peekBytesAlloc(alloc: std.mem.Allocator, cpu: anytype, addr: u16, size: usize) ![]u8 {
+pub fn peekBytesAlloc(alloc: std.mem.Allocator, mem: anytype, addr: u16, size: usize) ![]u8 {
     const buf = try alloc.alloc(u8, size);
     errdefer alloc.free(buf);
-    peekBytes(cpu, addr, buf);
+    peekBytes(mem, addr, buf);
     return buf;
 }
 
-pub fn peekString(alloc: std.mem.Allocator, cpu: anytype, addr: u16, sentinel: u8) !std.ArrayList(u8) {
+pub fn peekString(alloc: std.mem.Allocator, mem: anytype, addr: u16, sentinel: u8) !std.ArrayList(u8) {
     var buf = try std.ArrayList(u8).initCapacity(alloc, 256);
     errdefer buf.deinit();
 
     var offset: u16 = 0;
     while (true) {
-        const byte = cpu.peek8(@intCast(addr + offset));
+        const byte = mem.peek8(@intCast(addr + offset));
         if (byte == sentinel) break;
         try buf.append(byte);
         offset += 1;
