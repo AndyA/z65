8441be6c2e0f77f846b171a5a3e40d6f1bac9a47
Andy Armstrong <andy@hexten.net>
2025-07-25 15:44:40 +0100

    More help.

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
+# hibasic: Acorn BBC HiBASIC
+
+```
+Usage:
+    hibasic [OPTIONS] <prog>
+
+Options:
+    -h, --help
+            Display this help and exit.
+
+        --full-help
+            Print more help.
+
+    -c, --chain
+            Do CHAIN "prog" instead of LOAD "prog".
+
+    -s, --sync
+            Auto load when prog changes on disc. Auto save when prog
+            changes in memory.
+
+    -q, --quit
+            Quit after running (*BYE).
+
+    -e, --exec <line>...
+            Lines of BBC Basic to run. May be used more than once to supply
+            multiple lines.
+
+    <prog>
+            Program to load or run (--chain). May be text source or BBC
+            Basic native.
+```
+
+## Notes
+
+Both native BBC Basic format source files and textual source files are supported by LOAD, SAVE and CHAIN. To work with textual source files use the extension .bbc when editing them. Any other extension - or no extension means BBC Basic native format.
+
+There's currently no support for stopping a running program by hitting Escape and Ctrl-C will kill hibasic completely. Fixes for both are planned.
+
+There's currently no VDU emulation - characters are sent as-is to stdout. On one hand that means you can change the terminal colour and print UTF-8 characters. On the other hand COLOUR etc don't work. I plan to add support for mapping BBC colours to terminal colours.
+
 https://github.com/qiongzhu/multiarch-on-aarch64
 
 ## TODO
@@ -7,34 +47,23 @@ https://github.com/qiongzhu/multiarch-on-aarch64
   - implement more functions
 - keyboard input handling
   - tty to cooked mode
-  - runs on its own thread
+  - runs on its own thread?
   - readline?
 - VDU emulations
   - png canvas
   - sdl
   - svg
+  - terminal colours
 - handle plain text basic
   - ~~read and write~~ ✔
-  - watch
 - CLI options
-  - load
-  - chain
+  - ~~load~~ ✔
+  - ~~chain~~ ✔
   - watch
-  - run commands and exit
+  - ~~run commands and exit~~ ✔
 - trace to file
-- integrate M-UTS
+  - currently goes to stderr; maybe that's fine?
+- ~~integrate M-UTS~~ ✘
 - OSCLI fixes
   - pick command based on first word
   - make optionality more formally correct?
-
-## CLI
-
-```
-z64 [OPTIONS] <prog>
-
-  -c, --chain <PROG>  CHAIN "prog"
-      --watch <PROG>  CHAIN "prog" when it changes
-  -q, --quit          Quit after running
-  -e          <CMD>   Execute BBC Basic. May be used more than once
-      --help
-```
--- src/main.zig
+++ src/main.zig
@@ -21,7 +21,7 @@ const Tube65C02 = machine.CPU(
     .{ .clear_decimal_on_int = true },
 );
 
-pub fn hiBasic(alloc: std.mem.Allocator, config: hb.HiBasicConfig) !void {
+fn hiBasic(alloc: std.mem.Allocator, config: hb.HiBasicConfig) !void {
     var r_buf: [256]u8 = undefined;
     var w_buf: [0]u8 = undefined;
     var r = std.fs.File.stdin().reader(&r_buf);
@@ -65,6 +65,53 @@ pub fn hiBasic(alloc: std.mem.Allocator, config: hb.HiBasicConfig) !void {
         }
     }
 }
+
+fn help(comptime params: anytype, full: bool) !void {
+    var w_buf: [1024]u8 = undefined;
+    var w = std.fs.File.stdout().writer(&w_buf);
+
+    try w.interface.print(
+        \\hibasic: Acorn BBC HiBASIC
+        \\
+        \\Usage:
+        \\    hibasic [OPTIONS] <prog>
+        \\
+        \\Options:
+        \\
+    ,
+        .{},
+    );
+
+    try clap.help(&w.interface, clap.Help, &params, .{ .max_width = 75 });
+    if (full) {
+        try w.interface.print(
+            \\
+            \\ Notes:
+            \\    Both native BBC Basic format source files and textual source files are 
+            \\    supported by LOAD, SAVE and CHAIN. To work with textual source files
+            \\    use the extension .bbc when editing them. Any other extension - or no
+            \\    extension means BBC Basic native format.
+            \\
+            \\    There's currently no support for stopping a running program by hitting
+            \\    Escape and Ctrl-C will kill hibasic completely. Fixes for both are
+            \\    planned.
+            \\
+            \\    There's currently no VDU emulation - characters are sent as-is to
+            \\    stdout. On one hand that means you can change the terminal colour
+            \\    and print UTF-8 characters. On the other hand COLOUR etc don't work.
+            \\    I plan to add support for mapping BBC colours to terminal colours.
+            \\
+            \\    Andy Armstrong <andy@hexten.net>
+            \\
+            \\
+        ,
+            .{},
+        );
+    }
+
+    try w.interface.flush();
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -76,12 +123,17 @@ pub fn main() !void {
     };
 
     const params = comptime clap.parseParamsComptime(
-        \\   -h, --help             Display this help and exit.
-        \\   -c, --chain            CHAIN "prog"
-        \\   -s, --sync             Auto load / save
-        \\   -q, --quit             Quit after running
-        \\   -e, --exec <line>...   Lines of BBC Basic to run
-        \\   <prog>                 Program to load
+        \\    -h, --help            Display this help and exit.
+        \\        --full-help       Print more help.
+        \\    -c, --chain           Do CHAIN "prog" instead of LOAD "prog".
+        \\    -s, --sync            Auto load when prog changes on disc.
+        \\                          Auto save when prog changes in memory.
+        \\    -q, --quit            Quit after running (*BYE).
+        \\    -e, --exec <line>...  Lines of BBC Basic to run. May be
+        \\                          used more than once to supply
+        \\                          multiple lines.
+        \\    <prog>                Program to load or run (--chain). May
+        \\                          be text source or BBC Basic native.
         \\
     );
 
@@ -95,11 +147,8 @@ pub fn main() !void {
     };
     defer res.deinit();
 
-    if (res.args.help != 0) {
-        try clap.helpToFile(.stdout(), clap.Help, &params, .{
-            .description_on_new_line = false,
-            .spacing_between_parameters = 0,
-        });
+    if (res.args.help != 0 or res.args.@"full-help" != 0) {
+        try help(params, res.args.@"full-help" != 0);
         return;
     }
 
