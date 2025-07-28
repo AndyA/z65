a9452d2c4e5d192136650622d21693bdb0c860c5
Andy Armstrong <andy@hexten.net>
2025-07-28 12:38:24 +0100

    Rename etc

--- src/main.zig
+++ src/main.zig
@@ -168,5 +168,5 @@ test {
     @import("std").testing.refAllDecls(@This());
     _ = @import("disassembler/disasm.zig");
     _ = @import("tools/word_wrap.zig");
-    _ = @import("basic/types.zig");
+    _ = @import("basic/types/fp.zig");
 }
