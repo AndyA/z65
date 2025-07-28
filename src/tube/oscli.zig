064218af4822a46be9f708eaf9df88d64d26db30
Andy Armstrong <andy@hexten.net>
2025-07-28 22:03:00 +0100

    */

--- src/tube/oscli.zig
+++ src/tube/oscli.zig
@@ -3,19 +3,19 @@ const star = @import("../tools/star_commands.zig");
 const constants = @import("constants.zig");
 
 const StarCommands = struct {
-    pub fn @"*CAT"(
-        cpu: anytype,
-        args: anytype,
-    ) !void {
+    const Self = @This();
+
+    pub fn @"*/ <name:[]u8>"(cpu: anytype, args: anytype) !void {
+        try Self.@"*RUN <name:[]u8>"(cpu, args);
+    }
+
+    pub fn @"*CAT"(cpu: anytype, args: anytype) !void {
         _ = cpu;
         _ = args;
         std.debug.print("Meow!\n", .{});
     }
 
-    pub fn @"*FX <A:u8> [,<X:u8> [,<Y:u8>]]"(
-        cpu: anytype,
-        args: anytype,
-    ) !void {
+    pub fn @"*FX <A:u8> [,<X:u8> [,<Y:u8>]]"(cpu: anytype, args: anytype) !void {
         cpu.A = args.A;
         cpu.X = args.X orelse 0;
         cpu.Y = args.Y orelse 0;
@@ -34,49 +34,36 @@ const StarCommands = struct {
         });
     }
 
-    pub fn @"*LOAD <name:[]u8> <start:u16x>"(
-        cpu: anytype,
-        args: anytype,
-    ) !void {
+    pub fn @"*LOAD <name:[]u8> <start:u16x>"(cpu: anytype, args: anytype) !void {
         _ = cpu;
         std.debug.print("*LOAD \"{s}\" {x}\n", .{ args.name, args.start });
     }
 
-    pub fn @"*EXEC <name:[]u8>"(
-        cpu: anytype,
-        args: anytype,
-    ) !void {
+    pub fn @"*RUN <name:[]u8>"(cpu: anytype, args: anytype) !void {
+        _ = cpu;
+        std.debug.print("*RUN \"{s}\"\n", .{args.name});
+    }
+
+    pub fn @"*EXEC <name:[]u8>"(cpu: anytype, args: anytype) !void {
         _ = cpu;
         std.debug.print("*EXEC \"{s}\"\n", .{args.name});
     }
 
-    pub fn @"*SPOOL <name:[]u8>"(
-        cpu: anytype,
-        args: anytype,
-    ) !void {
+    pub fn @"*SPOOL <name:[]u8>"(cpu: anytype, args: anytype) !void {
         _ = cpu;
         std.debug.print("*SPOOL \"{s}\"\n", .{args.name});
     }
 
-    pub fn @"*QUIT"(
-        cpu: anytype,
-        args: anytype,
-    ) !void {
+    pub fn @"*QUIT"(cpu: anytype, args: anytype) !void {
         _ = args;
         cpu.stop();
     }
 
-    pub fn @"*DUMP <start:u16x> <end:u16xr> "(
-        cpu: anytype,
-        args: anytype,
-    ) !void {
+    pub fn @"*DUMP <start:u16x> <end:u16xr> "(cpu: anytype, args: anytype) !void {
         try cpu.os.hexDump(cpu, args.start, args.end.resolve(args.start));
     }
 
-    pub fn @"*!<shell:[]u8*>"(
-        cpu: anytype,
-        args: anytype,
-    ) !void {
+    pub fn @"*!<shell:[]u8*>"(cpu: anytype, args: anytype) !void {
         _ = cpu;
         std.debug.print("shell {s}\n", .{args.shell});
     }
