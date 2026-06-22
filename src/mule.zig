8c64a4401a40a9081e953326169aecca0c4e210c
Andy Armstrong <andy@hexten.net>
2026-06-22 19:39:53 +0100

    Oh! It seems to work...

--- src/mule.zig
+++ src/mule.zig
@@ -1,19 +1,71 @@
 const std = @import("std");
+const Io = std.Io;
 const print = std.debug.print;
+const assert = std.debug.assert;
 const linen = @import("linen/linen.zig");
 
+const ReadResult = union(enum) {
+    input: error{ EndOfStream, ReadFailed }!u8,
+    timeout: error{Canceled}!void,
+    shutdown: error{Canceled}!void,
+};
+
+fn readWithTimeout(io: Io, reader: *Io.Reader, timeout: ?Io.Duration) !ReadResult {
+    var results: [10]ReadResult = undefined;
+    var select = Io.Select(ReadResult).init(io, &results);
+    defer _ = select.cancel();
+
+    // _ = reader;
+    select.async(.input, Io.Reader.takeByte, .{reader});
+
+    if (timeout) |to| {
+        select.async(.timeout, Io.Clock.Duration.sleep, .{
+            .{ .raw = to, .clock = .awake },
+            io,
+        });
+    }
+
+    return try select.await();
+}
+
 pub fn main(init: std.process.Init) !void {
-    const stdin = std.Io.File.stdin();
+    // Set up our I/O implementation.
+    var threaded: Io.Threaded = .init(init.gpa, .{});
+    defer threaded.deinit();
+    const io = threaded.io();
+
+    const stdin = Io.File.stdin();
     const term = try linen.Term.init(.{ .stdin = stdin });
     defer term.deinit();
 
-    var io_buf: [1]u8 = undefined;
+    // const clock: Io.Clock = .awake;
+    print("Hit space to quit\n", .{});
+
+    var io_buf: [32]u8 = undefined;
     var reader = stdin.reader(init.io, &io_buf);
+    // var last_ts: i64 = 0;
     while (true) {
-        var buf: [1]u8 = undefined;
-        _ = try reader.interface.readSliceShort(&buf);
-        print("{x:0>2} ", .{buf[0]});
-        if (buf[0] == 0x20) break;
+        const res = try readWithTimeout(io, &reader.interface, .fromSeconds(1));
+        switch (res) {
+            .input => |i| {
+                const c = try i;
+                print("\\x{x:0>2}", .{c});
+                if (c == 0x20) break;
+            },
+            .shutdown => {},
+            .timeout => print("* ", .{}),
+        }
+        // const c = try reader.interface.takeByte();
+        // const ts = clock.now(init.io).toMilliseconds();
+        // if (last_ts == 0) last_ts = ts;
+        // const elapsed = ts - last_ts;
+        // last_ts = ts;
+        // if (elapsed > 10) print("\n", .{});
+        // if (c == 0x20) break;
+        // if (std.ascii.isPrint(c))
+        //     print("{c}", .{c})
+        // else
+        //     print("\\x{x:0>2}", .{c});
     }
 
     print("Bye!\n", .{});
