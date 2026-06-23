38801f6b05f1b60a3a277e634939fd0b1eb67e83
Andy Armstrong <andy@hexten.net>
2026-06-23 12:02:44 +0100

    Remove unused.

--- src/linen/platform/POSIX.zig
+++ src/linen/platform/POSIX.zig
@@ -45,32 +45,3 @@ pub fn deinit(self: Self) void {
         std.log.err("{s}\n", .{errno_string});
     };
 }
-
-// * send the query
-// * advance the state machine until
-//   - either the cursor position has been reported
-//   - or we timeout (~20ms)
-
-const CSI = "\x1B[";
-pub fn getCursorPosition(self: Self, input: anytype) !void {
-    _ = self;
-    try input.writeAll(CSI ++ "6n");
-
-    // const State = enum(u8) { BYTE0, BYTE1, VPOS };
-    // var state: State = .BYTE0;
-    // var vpos: i32 = 0;
-    // while (true) {
-    //     const bytes = try input.readInput();
-    //     for (bytes) |b| {
-    //         switch (state) {
-    //             .BYTE0 => {
-    //                 if (b == 0x1b) state = .BYTE1 else return error.BadResponse;
-    //             },
-    //             .BYTE1 => {
-    //                 if (b == '[') state = .VPOS else return error.BadResponse;
-    //             },
-    //             .VPOS => {},
-    //         }
-    //     }
-    // }
-}
